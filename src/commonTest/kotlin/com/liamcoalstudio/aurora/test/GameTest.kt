551f0293b969285f8728e9e848abc31a2436e61d
Liam Cole <liam@liamiam.com>
2021-10-07 15:33:34 -0700

    add color test

--- src/commonTest/kotlin/com/liamcoalstudio/aurora/test/GameTest.kt
+++ src/commonTest/kotlin/com/liamcoalstudio/aurora/test/GameTest.kt
@@ -1,6 +1,7 @@
 package com.liamcoalstudio.aurora.test
 
 import com.liamcoalstudio.aurora.Vector2f
+import com.liamcoalstudio.aurora.Vector3f
 import com.liamcoalstudio.aurora.draw.DrawCollection
 import com.liamcoalstudio.aurora.game.Game
 import com.liamcoalstudio.aurora.game.run
@@ -116,4 +117,70 @@ class GameTest {
     }
 
     @Test fun drawGame_test() = DrawGame().run()
+
+    data class ColorfulGameVertex(
+        val vPosition: Vector2f,
+        val vColor: Vector3f
+    )
+
+    class ColorfulGame : Game() {
+        override fun WindowOpenBuilder.window() {
+            windowed(800, 600)
+            title("Colorful Game")
+        }
+
+        val shader by shader<ColorfulGameVertex> {
+            vertex("""
+                #version 150 core
+                
+                in vec2 vPosition;
+                in vec3 vColor;
+                
+                out vec3 fColor;
+                
+                void main() {
+                    gl_Position = vec4(vPosition, 0, 1);
+                    fColor = vColor;
+                }
+            """.trimIndent())
+
+            fragment("""
+                #version 150 core
+                
+                in vec3 fColor;
+                
+                out vec4 oColor;
+                
+                void main() {
+                    oColor = vec4(fColor, 1);
+                }
+            """.trimIndent())
+
+            input(ColorfulGameVertex::vPosition, ShaderInputType.Vector2f)
+            input(ColorfulGameVertex::vColor, ShaderInputType.Vector3f)
+        }
+
+        private var draw: DrawCollection<ColorfulGameVertex>? = null
+
+        override fun init(window: WindowHandle) {
+            build(shader)
+            draw = shader.get().constructDrawCollection(
+                ColorfulGameVertex(Vector2f(0f, 0f), Vector3f(1f, 0f, 0f)),
+                ColorfulGameVertex(Vector2f(0f, 1f), Vector3f(0f, 1f, 0f)),
+                ColorfulGameVertex(Vector2f(1f, 0f), Vector3f(0f, 0f, 1f))
+            )
+        }
+
+        override fun draw(window: WindowHandle) {
+            shader.get().use()
+            draw?.draw()
+        }
+
+        override fun quit(window: WindowHandle) {
+            draw?.delete()
+            shader.get().delete()
+        }
+    }
+
+    @Test fun colorfulGame_test() = ColorfulGame().run()
 }
