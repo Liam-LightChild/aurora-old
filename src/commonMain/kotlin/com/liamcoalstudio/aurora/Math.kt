8385f78ef26cd83ba1303c1414d735303094d868
Liam Cole <liam@liamiam.com>
2021-10-07 20:38:30 -0700

    add matrices (sort of)

--- src/commonMain/kotlin/com/liamcoalstudio/aurora/Math.kt
+++ src/commonMain/kotlin/com/liamcoalstudio/aurora/Math.kt
@@ -16,3 +16,7 @@ typealias Vector4ui = Vector4<UInt>
 typealias Vector2d = Vector2<Double>
 typealias Vector3d = Vector3<Double>
 typealias Vector4d = Vector4<Double>
+
+data class Matrix2x2<T>(val x: Vector2<T>, val y: Vector2<T>)
+data class Matrix3x3<T>(val x: Vector3<T>, val y: Vector3<T>, val z: Vector3<T>)
+data class Matrix4x4<T>(val x: Vector4<T>, val y: Vector4<T>, val z: Vector4<T>, val w: Vector4<T>)
